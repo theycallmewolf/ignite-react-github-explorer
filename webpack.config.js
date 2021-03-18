1395f4a5fc554171b2fd068158be751e5a76ebdb
bruno-wolf <bruno.m.lobato@gmail.com>
2021-03-18 20:54:31 +0000

    HtmlWebpackPlugin config

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -21,4 +22,9 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html'),
+    }),
+  ],
 };
