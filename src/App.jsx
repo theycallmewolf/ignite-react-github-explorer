b57eaeb06cc1b703ca07d7b1e243f8b3c04e6b85
bruno-wolf <bruno.m.lobato@gmail.com>
2021-03-20 11:21:06 +0000

    webpack config sass

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
+import './styles/global.scss';
+
+
 export function App() {
 
-  throw new Error('Duuude!! You have broken all this crapy app');
+  // throw new Error('Duuude!! You have broken all this crapy app');
 
   return <h1>Hello Master Wolf!</h1>
 }
